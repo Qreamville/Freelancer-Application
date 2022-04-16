72a29ec590146ebd60f6917a6c67e83074d3bc35
Qreamville <Oyeleyebalikis@yahoo.com>
2022-04-15 23:14:34 -1200

    Started navbar

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./navigation.css"
+import { Navbar } from 'react-bootstrap'
+
+const Navigation = () => {
+  return (
+    <Navbar>
+      <Navbar.Brand>
+        Payme
+      </Navbar.Brand>
+    </Navbar>
+  )
+}
+
+export default Navigation
