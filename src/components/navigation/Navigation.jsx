b70ebad1c9432d8f24851bec4d2a35c381bfd16f
Qreamville <Oyeleyebalikis@yahoo.com>
2022-04-16 02:31:54 -1200

    completed navbar

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import "./navigation.css"
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 
 const Navigation = () => {
   return (
-    <Navbar>
+    <Navbar expand="lg" >
       <Navbar.Brand>
         Payme
       </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-collapse-id" />
+      <Navbar.Collapse id="navbar-collapse-id">
+        <Nav>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href='#testimonials'>Testimonials</Nav.Link>
+          <Nav.Link href='#contact'>Contact</Nav.Link>
+        </Nav>
+        <Nav className='ms-auto'>
+          <Nav.Link >Sign in</Nav.Link>
+          <Nav.Link className='nav-sign btn btn-outline-primary'>Sign up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
+
   )
 }
 
